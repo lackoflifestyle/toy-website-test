8083ae61eef2b0fe083b33edf69b6b781971d9bf
Marcus Larsson <bayareaviking@hotmail.com>
2025-01-15 18:57:55 -0800

    Create setup.ps1 file

---
+++ setup.ps1
@@ -0,0 +1,27 @@
+$githubOrganizationName = '<myGithubUser>'
+$githubRepositoryName = 'toy-website-test'
+$rg = "ToyWebsiteTest"
+$location = "westus2"
+
+$applicationRegistration = New-AzADApplication -DisplayName 'toy-website-test'
+New-AzADAppFederatedCredential -Name 'toy-website-test' `
+    -ApplicationObjectId $applicationRegistration.Id `
+    -Issuer 'https://token.actions.githubusercontent.com' `
+    -Audience 'api://AzureADTokenExchange' `
+    -Subject "repo:$($githubOrganizationName)/$($githubRepositoryName):environment:Website"
+
+New-AzADAppFederatedCredential -Name 'toy-website-test-branch' `
+    -ApplicationObjectId $applicationRegistration.Id `
+    -Issuer 'https://token.actions.githubusercontent.com' `
+    -Audience 'api://AzureADTokenExchange' `
+    -Subject "repo:$($githubOrganizationName)/$($githubRepositoryName):ref:refs/heads/main"
+
+$resourceGroup = New-AzResourceGroup -Name $rg -Location $location
+
+New-AzADServicePrincipal -AppId $($applicationRegistration.AppId)
+New-AzRoleAssignment -ApplicationId $($applicationRegistration.AppId) -RoleDefinitionName Contributor -Scope $($resourceGroup.ResourceId)
+
+$azureContext = Get-AzContext
+Write-Host "AZURE_CLIENT_ID: $($applicationRegistration.AppId)"
+Write-Host "AZURE_TENANT_ID: $($azureContext.Tenant.Id)"
+Write-Host "AZURE_SUBSCRIPTION_ID: $($azureContext.Subscription.Id)"
